f991fec2925b0bc6737df24c77d6398e425c4b5b
Michał Staromiejski <michal.staromiejski@gmail.com>
2024-09-11 18:11:28 +0200

    Locally generated algebras - basic facts about locality.

--- LocalRings/Defs.lean
+++ LocalRings/Defs.lean
@@ -2,6 +2,7 @@ import Mathlib.Algebra.Algebra.Defs
 import Mathlib.Algebra.Algebra.Subalgebra.Basic
 import Mathlib.Algebra.Field.Defs
 import Mathlib.Algebra.Group.Units.Hom
+import Mathlib.Data.Set.Basic
 import Mathlib.RingTheory.LocalRing.Defs
 import Mathlib.RingTheory.LocalRing.RingHom.Basic
 import Mathlib.RingTheory.Ideal.QuotientOperations
@@ -12,7 +13,7 @@ variable {R : Type*}
 variable [CommRing R]
 
 /-- Product of two non-trivial rings is not a local ring. -/
-theorem nonLocalRing_if_prod_of_2 {S : Type*} [CommRing S] [Nontrivial R] [Nontrivial S] :
+theorem nonLocalRing_if_prod_of_2 (R S : Type u) [CommRing R] [CommRing S] [Nontrivial R] [Nontrivial S] :
     ¬LocalRing (R × S) := by
   let a : R × S := ⟨1, 0⟩
   have hnua : ¬IsUnit a := by
@@ -29,42 +30,44 @@ lemma nonLocalDef [Nontrivial R] (h : ¬LocalRing R) : ∃ a : R, ¬IsUnit a ∧
   simp [not_exists, ←not_or] at hn
   exact h (LocalRing.of_isUnit_or_isUnit_one_sub_self hn)
 
+lemma nonLocalProj {R : Type u} [CommRing R] [Nontrivial R] (h : ¬LocalRing R) :
+    ∃ (K₁ K₂ : Type u) (fK₁ : Field K₁) (fK₂ : Field K₂) (f : R →+* K₁ × K₂), Function.Surjective f := by
+  /- get two maximal ideals and project -/
+  obtain ⟨a, ⟨hnua, hnub⟩⟩ := nonLocalDef h
+  have hnua1 : a ∈ nonunits R := by rwa [←mem_nonunits_iff] at hnua
+  have hnub1 : 1 - a ∈ nonunits R := by rwa [←mem_nonunits_iff] at hnub
+  obtain ⟨m₁, ⟨_, ham⟩⟩ := exists_max_ideal_of_mem_nonunits hnua1
+  obtain ⟨m₂, ⟨_, hbm⟩⟩ := exists_max_ideal_of_mem_nonunits hnub1
+  have coprime : IsCoprime m₁ m₂ := by
+    rw [Ideal.isCoprime_iff_exists]
+    use a, ham, 1 - a, hbm
+    ring
+  -- `R →+* R ⧸ m₁ ⊓ m₂`
+  let g := Ideal.Quotient.mk (m₁ ⊓ m₂)
+  -- `R ⧸ m₁ ⊓ m₂ ≃+* (R ⧸ m₁) × R ⧸ m₂`
+  let e := Ideal.quotientInfEquivQuotientProd m₁ m₂ coprime
+  let K₁ := R ⧸ m₁
+  let K₂ := R ⧸ m₂
+  let fK₁ : Field K₁ := Ideal.Quotient.field m₁
+  let fK₂ : Field K₂ := Ideal.Quotient.field m₂
+  let f := RingHom.comp (e : R ⧸ m₁ ⊓ m₂ →+* K₁ × K₂) g
+  use K₁, K₂, fK₁, fK₂, f
+  apply Function.Surjective.comp e.surjective Ideal.Quotient.mk_surjective
+
 /-- A ring `R` is local iff all ring homomorphism from `R` to a product of two fields
     are not surjective. -/
 theorem nonLocalRing_iff_image_prod_of_2_fields {R : Type u} [CommRing R] [Nontrivial R] :
-    (∀ {K₁ K₂ : Type u} [Field K₁] [Field K₂] (f : R →+* K₁ × K₂), ¬Function.Surjective f) ↔ LocalRing R := by
+    (∀ (K₁ K₂ : Type u) [Field K₁] [Field K₂] (f : R →+* K₁ × K₂), ¬Function.Surjective f) ↔ LocalRing R := by
   apply Iff.intro
-  · /- get two maximal ideals and project -/
-    intro h
+  · intro h
     by_contra hNonLocalR
-    obtain ⟨a, ⟨hnua, hnub⟩⟩ := nonLocalDef hNonLocalR
-    have hnua1 : a ∈ nonunits R := by rwa [←mem_nonunits_iff] at hnua
-    have hnub1 : 1 - a ∈ nonunits R := by rwa [←mem_nonunits_iff] at hnub
-    obtain ⟨m₁, ⟨hm1max, ham⟩⟩ := exists_max_ideal_of_mem_nonunits hnua1
-    obtain ⟨m₂, ⟨hm2max, hbm⟩⟩ := exists_max_ideal_of_mem_nonunits hnub1
-    have coprime : IsCoprime m₁ m₂ := by
-      rw [Ideal.isCoprime_iff_exists]
-      use a, ham, 1 - a, hbm
-      ring
-    let e := Ideal.quotientInfEquivQuotientProd m₁ m₂ coprime
-    have he := e.surjective
-    let g := Ideal.Quotient.mk (m₁ ⊓ m₂)
-    have hg : Function.Surjective g := Ideal.Quotient.mk_surjective
-    -- let K₁ := R / m₁ -- does not work?
-    let K₁ := @HasQuotient.Quotient R (Ideal R) Ideal.instHasQuotient m₁
-    let K₂ := @HasQuotient.Quotient R (Ideal R) Ideal.instHasQuotient m₂
-    let fK₁ : Field K₁ := Ideal.Quotient.field m₁
-    let fK₂ : Field K₂ := Ideal.Quotient.field m₂
-    let f := RingHom.comp (e : R ⧸ m₁ ⊓ m₂ →+* K₁ × K₂) g
-    have hf := Function.Surjective.comp he hg -- f is surjective
-    have contra_hf := @h K₁ K₂ fK₁ fK₂ f -- f is not surjective
-    rw [RingHom.coe_comp, RingHom.coe_coe] at contra_hf
-    exact contra_hf hf
+    obtain ⟨K₁, K₂, fK₁, fK₂, f, hf⟩ := nonLocalProj hNonLocalR
+    exact @h K₁ K₂ fK₁ fK₂ f hf
   · intro hlocalR
     by_contra h
     push_neg at h
-    obtain ⟨f, hfs⟩ := h
-    exact nonLocalRing_if_prod_of_2 (LocalRing.of_surjective' f hfs)
+    obtain ⟨K₁, K₂, _, _, f, hfs⟩ := h
+    exact nonLocalRing_if_prod_of_2 K₁ K₂ (LocalRing.of_surjective' f hfs)
 
 /-- An element `a` of a ring `R` is *local* iff
     it belongs to a local subring of `R`. -/
@@ -100,14 +103,13 @@ theorem local_if_all_local [Nontrivial R] (ha : ∀ a : R, isLocalElement a) : L
   obtain ⟨B, ⟨hB, haB⟩⟩ := ha a
   -- if `a` is a unit in `B`, then it is a unit in `R`
   -- if `1 - a` is a unit in `B`, then it is a unit in `R`
-  --apply LocalRing.isUnit_or_isUnit_one_sub_self
   obtain ⟨aa, haa⟩ := subring_rep haB
   have hbb : (1 - aa : B) = 1 - a := by simp [haa]
   exact Or.imp (unit_sub haa) (unit_sub hbb) (by apply LocalRing.isUnit_or_isUnit_one_sub_self)
 
 /-- A homomorphism of rings maps local elements to local elements. -/
-theorem isLocal_of_map_local {S : Type*} [Nontrivial S] [CommRing S]
-    {a : R} (ha : isLocalElement a) (f : R →+* S) : isLocalElement (f a) := by
+theorem isLocal_of_map_local {S : Type*} [Nontrivial S] [CommRing S] (f : R →+* S)
+    {a : R} (ha : isLocalElement a) : isLocalElement (f a) := by
   -- B := local ring containing a
   obtain ⟨B, ⟨hB, haB⟩⟩ := ha
   -- g : ↥B →+* S
@@ -130,3 +132,64 @@ theorem scalar_is_local {a : A} [Nontrivial A] (ha : a ∈ (⊥ : Subalgebra F A
   apply And.intro
   · exact (Algebra.botEquivOfInjective (algebraMap F A).injective).toRingEquiv.symm.localRing
   · assumption
+
+/-- An `F`-algebra `A` is *locally generated* if the local elements of `A`
+    generate `A` as a vector space over `F`. -/
+def isLocallyGenerated (F A : Type*) [Field F] [CommRing A] [Algebra F A] : Prop :=
+  Submodule.span F {a : A | isLocalElement a} = ⊤
+
+/- Auxilliary. TODO: move to `Utils.lean`? -/
+lemma span_transfer {R M₁ M₂ : Type*} [CommRing R] [AddCommMonoid M₁] [AddCommMonoid M₂]
+    [Module R M₁] [Module R M₂] {S₁ : Set M₁} {S₂ : Set M₂} {f : M₁ →ₗ[R] M₂}
+    (h : f '' S₁ ⊆ S₂) : (Submodule.span R S₁).map f ≤ Submodule.span R S₂ := by
+  have h1 : Submodule.span R (f '' S₁) ≤ Submodule.span R S₂ := Submodule.span_mono h
+  have h2 : (Submodule.span R S₁).map f ≤ Submodule.span R (f '' S₁) := by
+    rw [Submodule.map_span_le]
+    intro m hm
+    exact (Submodule.subset_span : f '' S₁ ⊆ Submodule.span R (f '' S₁)) (Set.mem_image_of_mem f hm)
+  exact Set.Subset.trans h2 h1
+
+/-- If `F`-algebra `A` is locally generated and `f : A → B` is a surjective `F`-algebra
+    homomorphism, then `F`-algebra `B` is also locally generated. -/
+lemma locallyGenerated_surjective {B : Type*} [CommRing B] [Nontrivial B] [Algebra F B] (f : A →ₐ[F] B)
+    (hf : Function.Surjective f) (h : isLocallyGenerated F A) : isLocallyGenerated F B := by
+  let lA := {a : A | isLocalElement a}
+  let lB := {b : B | isLocalElement b}
+  have h1 : f '' lA ⊆ lB := by
+    intro y hy
+    obtain ⟨x, ⟨hx, hfxy⟩⟩ := hy
+    rw [Set.mem_setOf_eq, ← hfxy] at *
+    exact isLocal_of_map_local f.toRingHom hx
+  have h2 : (Submodule.span F lA).map f ≤ Submodule.span F lB :=
+    span_transfer (f := f.toLinearMap) h1
+  rwa [h, Submodule.map_top, LinearMap.range_eq_top.mpr hf, top_le_iff] at h2
+
+/-- Transfer `R`-algebra structure via ring homomorphism. -/
+def algebra_fromRingHom {R A B : Type*} [CommSemiring R] [CommSemiring A] [CommSemiring B]
+    [Algebra R A] (f : A →+* B) : Algebra R B :=
+  (f.comp (algebraMap R A)).toAlgebra
+
+/-- A non-local `F`-algebra is locally generated iff there are two field extensions
+    `F ⊆ K₁, K₂` such that the `F`-algebra `K₁ × K₂` is locally generated. -/
+theorem nonlocal_locally_generated_iff_2_fields_prod:
+  (∀ {K₁ K₂ : Type u} [Field K₁] [Field K₂] [Algebra F K₁] [Algebra F K₂],
+    ¬isLocallyGenerated F (K₁ × K₂)) ↔ (∀ {A : Type u} [CommRing A] [Algebra F A] [Nontrivial A], isLocallyGenerated F A → LocalRing A) := by
+  constructor
+  · intro hkk A _ _ _ h
+    rw [←nonLocalRing_iff_image_prod_of_2_fields]
+    intro K₁ K₂ _ _ f
+    -- Introduce compatible `F`-algebra structures
+    let algKK : Algebra F (K₁ × K₂) := algebra_fromRingHom f
+    let algK₁ : Algebra F K₁ := algebra_fromRingHom (RingHom.fst K₁ K₂)
+    let algK₂ : Algebra F K₂ := algebra_fromRingHom (RingHom.snd K₁ K₂)
+    -- Promote `f` to an `F`-algebra homomorphism.
+    let f' : A →ₐ[F] (K₁ × K₂) := by
+      apply AlgHom.mk' f
+      intro c x
+      simp_all [Algebra.smul_def]
+      rfl
+    by_contra hf
+    exact hkk (K₁ := K₁) (K₂ := K₂) (locallyGenerated_surjective f' hf h)
+  · intro h K₁ K₂ _ _ _ _
+    by_contra hkk
+    exact nonLocalRing_if_prod_of_2 K₁ K₂ (h hkk)
